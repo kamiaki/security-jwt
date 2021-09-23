458380d1a634e5ce0b4e488f3028d057e9838552
wang_ying11 <7393654+wang_ying11@user.noreply.gitee.com>
2021-09-23 08:53:15 +0800

    123

--- src/main/java/com/zolvces/securityjwt/security/simple/JwtLoginFilter.java
+++ src/main/java/com/zolvces/securityjwt/security/simple/JwtLoginFilter.java
@@ -11,12 +11,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
+ * JwtLoginFilter 自定义的登录过滤器,把它加到SpringSecurity的过滤链中,拦截登录请求它干的事有
+ * 1 设置登录的url,请求的方式,其实也就是定义这个过滤器要拦截哪个请求
+ * 2 调用JwtAuthenticationProvider进行登录校验
+ * 3 校验成功调用LoginSuccessHandler,校验失败调用LoginSuccessHandler
  * @author niXueChao
  * @date 2019/4/2 14:07.
  */
 public class JwtLoginFilter extends AbstractAuthenticationProcessingFilter {
 
-
+    /**
+     * 设置登录的url,请求的方式,其实也就是定义这个过滤器要拦截哪个请求
+     */
     JwtLoginFilter() {
         super(new AntPathRequestMatcher("/jwtLogin", "GET"));
     }
