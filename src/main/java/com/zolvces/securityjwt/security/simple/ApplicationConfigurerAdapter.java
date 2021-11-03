3b31f8ada4cbe8dbf80ff0dc6e79a5491dfdc490
aki <247557768@qq.com>
2021-11-03 16:02:40 +0800

    这就是 优化了过期时间 和 刷新

--- src/main/java/com/zolvces/securityjwt/security/simple/ApplicationConfigurerAdapter.java
+++ src/main/java/com/zolvces/securityjwt/security/simple/ApplicationConfigurerAdapter.java
@@ -1,9 +1,9 @@
 package com.zolvces.securityjwt.security.simple;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,7 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 //////////////////////////////////////////
 //////////////////////////////////////////
 /**
- *  spring security核心
+ * spring security核心
  */
 //////////////////////////////////////////
 //////////////////////////////////////////
@@ -37,6 +37,9 @@ public class ApplicationConfigurerAdapter extends WebSecurityConfigurerAdapter {
     @Autowired
     private JwtUserDetailServiceImpl jwtUserDetailService;
 
+    @Autowired
+    @Qualifier(value = "myPassWordEncoder")
+    private PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -53,7 +56,7 @@ public class ApplicationConfigurerAdapter extends WebSecurityConfigurerAdapter {
         //登录过滤器的授权提供者(就这么叫吧)
         //调用JwtAuthenticationProvider进行登录校验
         JwtAuthenticationProvider provider = new JwtAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder());
+        provider.setPasswordEncoder(passwordEncoder);
         provider.setUserDetailsService(jwtUserDetailService);
 
         //JWT校验过滤器
@@ -67,7 +70,7 @@ public class ApplicationConfigurerAdapter extends WebSecurityConfigurerAdapter {
                 //会进行判断SecurityContext是否有凭证(Authentication),若前面的过滤器都没有提供凭证,
                 //匿名过滤器会给SecurityContext提供一个匿名的凭证(可以理解为用户名和权限为anonymous的Authentication),
                 //这也是JwtHeadFilter发现请求头中没有jwtToken不作处理而直接进入下一个过滤器的原因
-            .exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
+                .exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
             response.setContentType("application/json;charset=UTF-8");
             response.getWriter().write("需要登陆");
         })
@@ -94,7 +97,7 @@ public class ApplicationConfigurerAdapter extends WebSecurityConfigurerAdapter {
     }
 
 
-    @Bean
+    @Bean(name = "myPassWordEncoder")
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
